ed191c6007c99e31a4c4998920aefde12dec2f67
Ellen Cooney <elleclarec@gmail.com>
2023-06-13 16:33:11 +0100

    Refactor: Favourite movie list now stored in a context.

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,22 +21,23 @@ const queryClient = new QueryClient({
   },
 });
 
-
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <SiteHeader /> 
-      <Routes>
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-      </Routes>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <SiteHeader />
+          <MoviesContextProvider>
+            <Routes>
+              <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+              <Route path="/movies/:id" element={<MoviePage />} />
+              <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+              <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+            </Routes>
+        </MoviesContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
