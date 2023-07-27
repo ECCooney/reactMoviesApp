08e450a49bb78f16b686bab941c835bb1d16306a
Ellen Cooney <elleclarec@gmail.com>
2023-07-27 16:31:31 +0100

    Pagination implemented on upcoming movies

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -4,28 +4,49 @@ import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import MustWatchIcon from "../components/cardIcons/mustWatch";
+import Pagination from '@mui/material/Pagination';
 
 
 const UpcomingMoviesPage = (props) => {
-    const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    if (isLoading) {
-      return <Spinner />;
-    }
-    if (isError) {
-      return <h1>{error.message}</h1>;
-    }
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+    console.log(value)
+  };
+
+    const {  data, error, isLoading, isError }  = useQuery(['discover', currentPage], () => getUpcomingMovies(currentPage), { keepPreviousData: true })
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
   
     const upcomingMovies = data ? data.results : [];
 
   return (
+    <>
     <PageTemplate
       title='Upcoming Movies'
       movies={upcomingMovies}
       action={(movie) => {
         return <MustWatchIcon movie={movie} />
-      }}
+      }}      
     />
+    <Pagination
+        count='100'
+        variant='outlined'
+        color='primary'
+        shape="rounded"
+        showFirstButton 
+        showLastButton
+        className='pagination'
+        page={currentPage}
+        onChange={handleChange}
+      />
+      </>
   );
 };
 
